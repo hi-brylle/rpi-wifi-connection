286ea5b074ddf336d40ca43d51dffc93c3a62b7c
Brylle <kiirbritmiin@gmail.com>
2024-08-13 14:54:06 +0800

    Update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     entry: ['./src/index.ts'],
     dts: true,
     shims: true,
-    skipNodeModulesBundle: true,
+    splitting: false,
+    sourcemap: true,
     clean: true,
 });
